3b8408a43a3e865a269dab72bd31a9121385da35
Alyssa Pratt <alyssakpratt@gmail.com>
2020-12-05 17:41:39 -0600

    updates

--- template/app.js
+++ template/app.js
@@ -1,6 +1,6 @@
 
 new Typed('#text-span', {
-    strings: [':)','lyssa Hardwick', 'n artist', '_developer', 'n animal lover', '_designer', '_self learner', '_creative' ],
+    strings: [':)','lyssa Hardwick', 'n artist','_gamer', '_developer', 'n animal lover', '_designer', '_self learner', '_creative' ],
     typeSpeed: 80,
     backSpeed: 40,
     cursorChar: '_',
